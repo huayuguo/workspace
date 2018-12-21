e182674f2456a4bf694039291d1090106e1dba6d
huangchao <huangchao@19zn.cc>
2018-12-21 18:22:55 +0800

    修改自动灭屏测试APP

--- AndroidStudio/TestDim2/app/src/main/java/com/hcn/huangchao/testdim2/MainActivity.java
+++ AndroidStudio/TestDim2/app/src/main/java/com/hcn/huangchao/testdim2/MainActivity.java
@@ -9,38 +9,37 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.content.Context;
+import android.widget.Switch;
 
 import com.hcn.huangchao.testdim2.R;
 
 public class MainActivity extends AppCompatActivity {
 
-    private void setBrightnessMode(Context context,int mode) {
-        try {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                if (Settings.System.canWrite(context)) {
-                    Settings.System.putInt(context.getContentResolver(), Settings.System.SCREEN_BRIGHTNESS_MODE, mode);
-                } else {
-                    Intent intent = new Intent(android.provider.Settings.ACTION_MANAGE_WRITE_SETTINGS);
-                    intent.setData(Uri.parse("package:" + context.getPackageName()));
-                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                    context.startActivity(intent);
-                }
-            } else {
-                Settings.System.putInt(context.getContentResolver(), Settings.System.SCREEN_BRIGHTNESS_MODE, mode);
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (!Settings.System.canWrite(MainActivity.this)) {
+                Intent intent = new Intent(android.provider.Settings.ACTION_MANAGE_WRITE_SETTINGS);
+                intent.setData(Uri.parse("package:" + MainActivity.this.getPackageName()));
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                MainActivity.this.startActivity(intent);
+            }
+        }
+
         Button mButton = (Button) findViewById(R.id.button);
         final EditText mEdit = (EditText) findViewById(R.id.editView);
+        Switch swEnable = findViewById(R.id.swEnable);
+
+        int s =  Settings.System.getInt(getContentResolver(), "lightdim_setting", 0);
+        mEdit.setText("" + s / 1000);
+        int c =  Settings.System.getInt(getContentResolver(), "lightdim_onoff", 0);
+        swEnable.setChecked(c == 1);
 
         mButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -59,33 +58,27 @@ public class MainActivity extends AppCompatActivity {
 
                 int second = Integer.parseInt(mEdit.getText().toString());
                 try {
-                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                        if (Settings.System.canWrite(MainActivity.this)) {
-                            if(second != 0) {
-                                Settings.System.putInt(getContentResolver(), "lightdim_onoff", 1);
-                                Settings.System.putInt(getContentResolver(), "lightdim_setting", second * 1000);
-                            } else {
-                                Settings.System.putInt(getContentResolver(), "lightdim_onoff", 0);
-                            }
-                        } else {
-                            Intent intent = new Intent(android.provider.Settings.ACTION_MANAGE_WRITE_SETTINGS);
-                            intent.setData(Uri.parse("package:" + MainActivity.this.getPackageName()));
-                            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                            MainActivity.this.startActivity(intent);
-                        }
-                    } else {
-                        if(second != 0) {
-                            Settings.System.putInt(getContentResolver(), "lightdim_onoff", 1);
-                            Settings.System.putInt(getContentResolver(), "lightdim_setting", second * 1000);
-                        } else {
-                            Settings.System.putInt(getContentResolver(), "lightdim_onoff", 0);
-                        }
+                    if(second >= 0) {
+                        Settings.System.putInt(getContentResolver(), "lightdim_setting", second * 1000);
                     }
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
+            }
+        });
 
-                //setBrightnessMode(MainActivity.this, 0);
+        swEnable.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                try {
+                    if(isChecked) {
+                        Settings.System.putInt(getContentResolver(), "lightdim_onoff", 1);
+                    } else {
+                        Settings.System.putInt(getContentResolver(), "lightdim_onoff", 0);
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
             }
         });
 
