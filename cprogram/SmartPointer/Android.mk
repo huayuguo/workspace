ef99ea60ebabc338590da9d45d781d39a934faa5
huangchao <huangchao@19zn.cc>
2019-01-07 16:29:32 +0800

    增加Android智能指针测试程序

---
+++ cprogram/SmartPointer/Android.mk
@@ -0,0 +1,18 @@
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE := weightpointer
+LOCAL_SRC_FILES := weightpointer.cpp
+LOCAL_SHARED_LIBRARIES := \
+        libcutils \
+        libutils
+include $(BUILD_EXECUTABLE)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE := lightpointer
+LOCAL_SRC_FILES := lightpointer.cpp
+LOCAL_SHARED_LIBRARIES := \
+        libcutils \
+        libutils
+include $(BUILD_EXECUTABLE)
